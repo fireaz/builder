c4507a6a2a04c73c5dd2a96f0175057f64af188e
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-05-23 22:31:11 +0700

    Update function.php

---
+++ helpers/function.php
@@ -0,0 +1,10 @@
+<?php
+
+use FireAZ\Builder\Facades\Menu;
+
+if (!function_exists('menu_render')) {
+    function menu_render($_position = '')
+    {
+        return Menu::render($_position);
+    }
+}
