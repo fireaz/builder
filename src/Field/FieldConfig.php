91dafffdabd67e20a4d1471291545a7f67cc6cc2
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-06-25 22:48:25 +0700

    Update FieldConfig.php

--- src/Field/FieldConfig.php
+++ src/Field/FieldConfig.php
@@ -13,6 +13,26 @@ class FieldConfig
     private $fieldLabel = 'Text';
     private $fieldTitle = 'Text';
     private $fieldName = 'Text';
+    private $fieldFormat;
+    public function __construct($name = '')
+    {
+        $this->fieldName = $name;
+    }
+    public function Format($format)
+    {
+        $this->fieldFormat = $format;
+        return $this;
+    }
+    public function Title($title)
+    {
+        $this->fieldTitle = $title;
+        return $this;
+    }
+    public function Label($label)
+    {
+        $this->fieldLabel = $label;
+        return $this;
+    }
     public function getFieldType()
     {
         return $this->fieldType;
